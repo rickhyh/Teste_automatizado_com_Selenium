a4ca421a88bc6233b04de00fac91b01e9d5f5466
rickhyh <icarogacarvalho@gmail.com>
2021-12-20 16:30:09 -0300

    Adição do @After e, correção do teste que não cumpria com os objetivos propostos no plano de teste

--- src/test/java/TestsFront/PageHome.java
+++ src/test/java/TestsFront/PageHome.java
@@ -2,6 +2,7 @@ package TestsFront;
 
 import java.util.concurrent.TimeUnit;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -17,20 +18,43 @@ public class PageHome {
 	public void Setup() {
 		System.setProperty("webdriver.chrome.driver", System.getenv("Driver"));
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		driver.get("https://online.sp.senai.br/");
 	}
 	
 	@Test
-	public void TesteNavegarParaGoogle() {
-		driver.get("https://online.sp.senai.br/");
+	public void Teste() {
 		
-		WebElement input = driver.findElement(By.id("Busca1_txtFiltro"));
+		//Foram criadas duas variáveis como um workaround, porque estava ocorrendo um bug que impedia o uso de uma variável mais de 2 vezes.
 		
-		input.sendKeys("Gestão");
-		//input.sendKeys(Keys.ENTER);
+		WebElement busca01;
+		WebElement busca02;
 		
-		driver.findElement(By.id("Busca1_btnBusca")).click();
+			try {
+				
+				
+				//Primeira pesquisa
+				busca01 = driver.findElement(By.name("ctl00$Busca1$txtFiltro"));
+				busca01.sendKeys("Gestão");
+				busca01.sendKeys(Keys.ENTER);
+				
+				//Tempo para apurar resultados.				
+				Thread.sleep(10000);
+				
+				//Segunda pesquisa
+				busca02 = driver.findElement(By.name("ctl00$Busca1$txtFiltro"));
+				busca02.clear();
+				busca02.sendKeys("Tecnologia");
+				driver.findElement(By.id("Busca1_btnBusca")).click();
+				Thread.sleep(10000);
+					} catch (InterruptedException exception) {
+						exception.printStackTrace();
+					}
+	}
+	
+	@After
+	public void EncerrarTeste() {
+		driver.close();
 	}
 }
-	
